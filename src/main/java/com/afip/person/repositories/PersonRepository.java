f3e1f531806358348ad9260aa84353df787e3af9
deltalogan <deltalogan@yahoo.com.ar>
2022-12-30 16:20:29 -0300

    first commit

--- src/main/java/com/afip/person/repositories/PersonRepository.java
+++ src/main/java/com/afip/person/repositories/PersonRepository.java
@@ -3,11 +3,9 @@ package com.afip.person.repositories;
 import java.util.ArrayList;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.afip.person.models.PersonModel;
 
-@Repository
 public interface PersonRepository extends CrudRepository<PersonModel, Long> {
 	public abstract ArrayList<PersonModel> findByCuil(String cuil);
 
